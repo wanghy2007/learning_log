fd988663b1d670cd034f8812771e9d0f84b12fc5
Hong-Yi Wang <hwang@Hong-Yis-MacBook-Air.local>
2022-04-08 19:01:19 -0700

    Added new topic

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Page that shows all topics.
     path('topics/', views.topics, name='topics'),
-    path('topics/<int:topic_id>/', views.topic, name='topic')
+    # Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic')
 ]
