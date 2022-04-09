7f80e30810d7405050fb26c608c28111e3ec2a2e
Hong-Yi Wang <hwang@Hong-Yis-MacBook-Air.local>
2022-04-08 18:44:53 -0700

    First commit

---
+++ learning_logs/urls.py
@@ -0,0 +1,14 @@
+"""Define URL patterns for learning_logs."""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+    # Page that shows all topics.
+    path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic')
+]
